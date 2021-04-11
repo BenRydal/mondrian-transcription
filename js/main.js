1d154f47ff8808b5f91d52c2c13b8cbdf410557b
BenRydal <ben@benrydal.com>
2021-04-11 13:03:34 -0400

    Update doc comments for main

--- js/main.js
+++ js/main.js
@@ -1,9 +1,29 @@
 /*
-CREDITS/LICENSE INFORMATION: This software is licensed under the GNU General Public License Version 2.0. See the GNU General Public License included with this software for more details. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. Mondrian Transcription Software originally developed by Ben Rydal Shapiro at Vanderbilt University as part of his dissertation titled Interaction Geography & the Learning Sciences. Copyright (C) 2018 Ben Rydal Shapiro, and contributers. To reference or read more about this work please see: https://etd.library.vanderbilt.edu/available/etd-03212018-140140/unrestricted/Shapiro_Dissertation.pdf
+// TO DO: will need small and large display video width/heights at program start
+// increase button
+// if not already increased...
+// set new displayvideo width/height
+// set new video width/height of html5 element itself
+// draw black background video rect at new width/height
+// draw image of video
+
+// descrease
+// if not already decreased
+// set new/small displayvideo width/height
+// set new video width/height of html5 element itself
+// draw black background video rect at new width/height
+// draw image of video
 */
 
-// COLUMN HEADERS FOR FILES
-const fileHeaders = ["time", "x", "y"];
+/*
+CREDITS/LICENSE INFORMATION: This software is licensed under the GNU General Public License Version 2.0. 
+See the GNU General Public License included with this software for more details. 
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details. 
+Mondrian Transcription Software originally developed by Ben Rydal Shapiro at Vanderbilt University as part of his 
+dissertation titled Interaction Geography & the Learning Sciences. Copyright (C) 2018 Ben Rydal Shapiro, and contributers. 
+To reference or read more about this work please see: https://etd.library.vanderbilt.edu/available/etd-03212018-140140/unrestricted/Shapiro_Dissertation.pdf
+*/
 
 // DATA
 let paths = []; // holds all recorded path files
@@ -11,8 +31,10 @@ let curPath; // current path to record
 let dataUpdate; // object that holds methods to update data recording and video
 let reSetAllData = true; // controls redrawing of data
 let curFileToOutput = 0; // current file number to write to output
-let frameAndSampleWhenStoppedRate = 30; // controls both frameRate of program and amount data is sampled when cursor is not moving when recording data
-// Path is conceptually a person's movement race and has x/y position and time lists 
+const frameAndSampleWhenStoppedRate = 30; // controls both frameRate of program and amount data is sampled when cursor is not moving when recording data
+const fileHeaders = ["time", "x", "y"]; // Column headers for outputted .CSV movement files
+
+// Represents the object being recorded such as person or thing
 class Path {
   xPos = [];
   yPos = [];
@@ -48,34 +70,45 @@ let keyTextSize, infoTextSize;
 let infoMsg = "MONDRIAN TRANSCRIPTION\nby Ben Rydal Shapiro & contributers\nbuilt with p5.js";
 let descMSG = "Hi there! This tool allows you to transcribe fine-grained positioning data from video. To get started, use the top buttons to upload a floor plan image file (PNG or JPG) and a video file (MP4). Then, use the key codes below to interact with the video and use your cursor to draw on the floor plan. As you interact with the video and simultaneously draw on the floor plan, positioning data is recorded as a CSV file organized by time in seconds and x/y pixel positions scaled to the pixel size of your floor plan image file. You can save this file anytime and then record another movement path.\n\nKEY CODES:\nPlay/Pause (p), Fast-Forward (f), Rewind (b), Reset (r), Save File (s)"
 
+/**
+ * @description Optional p5.js method, will complete before setup/draw begin
+ */
 function preload() {
   font_PlayfairItalic = loadFont("data/fonts/PlayfairDisplay-Italic.ttf");
   font_Lato = loadFont("data/fonts/Lato-Light.ttf");
 }
-
+/**
+ * @description Required p5.js method, here it sets canvas, GUI and initial drawing requirements
+ */
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight, P2D);
   frameRate(frameAndSampleWhenStoppedRate);
   setGUIWindows();
   drawGUIWindows();
   drawKeys();
-  curPath = new Path();
+  curPath = new Path(); // set initial path and UpdateData 
   dataUpdate = new UpdateData();
 }
 
-// Two modes for drawing when data is and is not all loaded
+/**
+ * @description Required p5.js method, here it organizes two drawing modes for when data is and is not loaded
+ */
 function draw() {
   if (floorPlanLoaded && movieLoaded) setDrawingScreen();
   else setLoadDataScreen();
 }
 
-// Organizes methods for recording once all data is loaded
+/**
+ * @description Organizes methods for recording once all data is loaded
+ */
 function setDrawingScreen() {
   if (reSetAllData) dataUpdate.reDrawAllData(); // Runs once after data is initially loaded or file is written
   if (recording) dataUpdate.prepareRecording(); // records data and updates visualization if in record mode
 }
 
-// Displays image or blank screen indicating movie is loaded
+/**
+ * @description Displays image or blank screen indicating movie is loaded
+ */
 function setLoadDataScreen() {
   if (floorPlanLoaded) image(floorPlan, displayFloorplanXpos, displayFloorplanYpos, displayFloorplanWidth, displayFloorplanHeight);
   else if (movieLoaded) {
