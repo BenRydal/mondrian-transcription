811273d63d9c954b6cb9f027870a393deb1b6d3e
BenRydal <ben@benrydal.com>
2021-04-14 23:11:43 -0400

    Update main comments, rmv preload

--- js/main.js
+++ js/main.js
@@ -9,23 +9,22 @@ To reference or read more about this work please see: https://etd.library.vander
 */
 
 // DATA
-let paths = []; // holds all recorded movement path files
-let curPath; // holds path object with lists of x/y positions and time values for current movement path
-let dataUpdate; // object that holds methods to synchronize updating of data recording and video
-let curFileToOutput = 0; // current file number to write to output
-const frameAndSampleWhenStoppedRate = 30; // controls both frameRate of program and amount data is sampled when cursor is not moving when recording data
-const fileHeaders = ["time", "x", "y"]; // Column headers for outputted .CSV movement files
-
-// Represents the object being recorded such as person or thing
+// Represents the object being recorded such as person or thing as lists of x/y pixel positions and time values
 class Path {
   xPos = [];
   yPos = [];
   tPos = [];
 }
+let paths = []; // holds all path objects created
+let curPath; // holds currently recorded path object
+let dataUpdate; // global object that represents instance of UpdateData class to control synchronized method calls for path and movie methods
+let curFileToOutput = 0; // current file number to write to output
+const frameAndSampleWhenStoppedRate = 30; // controls both frameRate of program and amount data is sampled when cursor is not moving when recording data
+const fileHeaders = ["time", "x", "y"]; // Column headers for outputted .CSV movement files
 
 // FLOOR PLAN
-let floorPlan; // floor plan display image file set by user uploaded image file
-let inputFloorPlanWidth, inputFloorPlanHeight; // User uploaded image file
+let floorPlan; // floor plan display image file set to user uploaded image file
+let inputFloorPlanWidth, inputFloorPlanHeight; // width/height of user uploaded image file
 
 // VIDEO
 let movie; // P5.js media element to display/interact with HTML5 video from file uploaded by user
@@ -54,17 +53,12 @@ let infoMsg = "MONDRIAN TRANSCRIPTION SOFTWARE\n\nby Ben Rydal Shapiro & contrib
 let showInfo = true;
 
 /**
- * Optional p5.js method, will complete before setup/draw begin
- */
-function preload() {
-  font_PlayfairItalic = loadFont("data/fonts/PlayfairDisplay-Italic.ttf");
-  font_Lato = loadFont("data/fonts/Lato-Light.ttf");
-}
-/**
  * Required p5.js method, sets canvas, GUI and initial drawing requirements
  */
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight, P2D);
+  font_PlayfairItalic = loadFont("data/fonts/PlayfairDisplay-Italic.ttf");
+  font_Lato = loadFont("data/fonts/Lato-Light.ttf");
   frameRate(frameAndSampleWhenStoppedRate);
   setGUIWindows();
   curPath = new Path(); // set initial path and UpdateData 
