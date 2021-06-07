a7ff6dfc25310a250c2dcf6ec75c4f7013ece40b
BenRydal <ben@benrydal.com>
2021-06-07 15:30:17 -0400

    Fix movie revoke URL

--- js/video-player.js
+++ js/video-player.js
@@ -19,16 +19,14 @@ class VideoPlayer {
         movie = createVideo(fileLocation, () => {
             // ADD VIDEO DURATION TEST HERE? console.log(movie.duration());
             movie.id('moviePlayer');
-            videoPlayer.setInputMovieSize(); // set global movie size constants
-            videoPlayer.setDisplayMovieSize(keys.displayVideoWidth, keys.displayVideoHeight);
             movie.hide();
             movie.stop(); // necessary to be able to draw starting frame before playing the video
-            URL.revokeObjectURL(movie.src);
             // Native P5 onended and duration methods don't seem to work, so use below 
             const mov = document.getElementById('moviePlayer');
-            mov.onended = function () { // When movie ends, set recording false
-                core.recording = false;
-            };
+            mov.onended = () => core.recording = false;
+            videoPlayer.setInputMovieSize(); // set global movie size constants
+            videoPlayer.setDisplayMovieSize(keys.displayVideoWidth, keys.displayVideoHeight);
+            movie.onload = () => URL.revokeObjectURL(this.src);
             core.movieLoaded = true;
             console.log("New Video Loaded");
             updateData.updateMovie.drawCurFrame(); // after loading video and restarting loop, draw starting frame to indicate movie is loaded
