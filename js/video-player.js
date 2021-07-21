a1c171f1fbae233336c623fc17a4feaf7617a82b
BenRydal <ben@benrydal.com>
2021-07-20 20:18:37 -0400

    Rmv unnecessary inputVideoSize width/height vars

--- js/video-player.js
+++ js/video-player.js
@@ -6,29 +6,16 @@ class VideoPlayer {
      */
 
     constructor(fileLocation, sketch) {
-        this.inputMovieWidth = null; // Original pixel size of video   
-        this.inputMovieHeight = null;
         this.reScaledMovieWidth = null; // Rescaled pixel size of video to fit display container
         this.reScaledMovieHeight = null;
         this.movieDiv = sketch.createVideo(fileLocation, () => {
-            // TODO: ADD VIDEO DURATION TEST HERE: console.log(movieDiv.duration());
             this.movieDiv.id('moviePlayer');
             this.movieDiv.hide();
-            this.setInputMovieSize(); // set global movie size constants
             this.setDisplayMovieSize(sketch.videoContainer.width, sketch.videoContainer.height);
             this.movieDiv.onload = () => URL.revokeObjectURL(fileLocation);
             document.getElementById('moviePlayer').onended = () => sketch.mediator.isRecording = false; // end program recording when movie ends
             sketch.mediator.newVideoLoaded();
         });
-    }
-
-    /**
-     * Sets global pixel width/height for movie file to scale size dynamically in program
-     * NOTE: get input movie width/height because if you resize video movieDiv width/height are lost/set to 0
-     */
-    setInputMovieSize() {
-        this.inputMovieWidth = this.movieDiv.width;
-        this.inputMovieHeight = this.movieDiv.height;
     }
 
     /**
@@ -39,13 +26,13 @@ class VideoPlayer {
      */
     setDisplayMovieSize(containerWidth, containerHeight) {
         let ratio = 0; // Used for aspect ratio
-        this.reScaledMovieWidth = this.inputMovieWidth; // set to equal input size in case first conditional is not triggered
-        this.reScaledMovieHeight = this.inputMovieHeight;
+        this.reScaledMovieWidth = this.movieDiv.width; // set to equal input size in case first conditional is not triggered
+        this.reScaledMovieHeight = this.movieDiv.height;
         // Check if input video pixel height is larger than display container, scale down if it is
-        if (this.inputMovieHeight > containerHeight) {
-            ratio = containerHeight / this.inputMovieHeight; // get ratio for scaling image, use tempHeight
-            this.reScaledMovieHeight = this.inputMovieHeight * ratio; // Reset height to match scaled image
-            this.reScaledMovieWidth = this.inputMovieWidth * ratio; // Reset width to match scaled image
+        if (this.movieDiv.height > containerHeight) {
+            ratio = containerHeight / this.movieDiv.height; // get ratio for scaling image, use tempHeight
+            this.reScaledMovieHeight = this.movieDiv.height * ratio; // Reset height to match scaled image
+            this.reScaledMovieWidth = this.movieDiv.width * ratio; // Reset width to match scaled image
         }
         // Then check if input/display video pixel width is still larger than display container, scale down if it is
         if (this.reScaledMovieWidth > containerWidth) {
