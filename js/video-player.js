f0a9118a7be7b5702f3f0a235445260fe829c04a
BenRydal <ben@benrydal.com>
2021-12-02 20:56:13 -0500

    Fix video scaling bug

--- js/video-player.js
+++ js/video-player.js
@@ -10,8 +10,8 @@ class VideoPlayer {
         this.scaledHeight = null;
         this.movieDiv = sketch.createVideo(fileLocation, () => {
             this.movieDiv.id('moviePlayer');
-            this.movieDiv.hide();
-            this.setDisplayMovieSize(sketch.videoContainer.width, sketch.videoContainer.height);
+            this.movieDiv.hide(); // hide html5 video element as program use p5 image drawing methods to draw video frames
+            [this.scaledWidth, this.scaledHeight] = this.scaleRectToBounds(this.movieDiv, sketch.videoContainer);
             this.movieDiv.onload = () => URL.revokeObjectURL(fileLocation);
             document.getElementById('moviePlayer').onended = () => sketch.mediator.isRecording = false; // end program recording when movie ends
             sketch.mediator.newVideoLoaded();
@@ -19,28 +19,16 @@ class VideoPlayer {
     }
 
     /**
-     * Sets pixel size to display video based on original input video size and container width/height parameters
-     * NOTE: temp width/height values are created/used because movie.width and movie.height cause issues
-     * @param  {} containerWidth
-     * @param  {} containerHeight
+     * Fits rectangle into another rectangle's bounds
+     * @param rect
+     * @param bounds
+     * @returns {width: Number, height: Number}
      */
-    setDisplayMovieSize(containerWidth, containerHeight) {
-        let ratio = 0; // Used for aspect ratio
-        this.scaledWidth = this.movieDiv.width; // set to equal input size in case first conditional is not triggered
-        this.scaledHeight = this.movieDiv.height;
-        // Check if input video pixel height is larger than display container, scale down if it is
-        if (this.movieDiv.height > containerHeight) {
-            ratio = containerHeight / this.movieDiv.height; // get ratio for scaling image, use tempHeight
-            this.scaledHeight = this.movieDiv.height * ratio; // Reset height to match scaled image
-            this.scaledWidth = this.movieDiv.width * ratio; // Reset width to match scaled image
-        }
-        // Then check if input/display video pixel width is still larger than display container, scale down if it is
-        if (this.scaledWidth > containerWidth) {
-            ratio = containerWidth / this.scaledWidth; // get ratio for scaling image
-            this.scaledHeight = this.scaledHeight * ratio; // Reset height to match scaled image
-            this.scaledWidth = this.scaledWidth * ratio; // Reset width to match scaled image
-        }
-        this.movieDiv.size(this.scaledWidth, this.scaledHeight); // set the element to the new width and height
+    scaleRectToBounds(rect, bounds) {
+        const rectRatio = rect.width / rect.height;
+        const boundsRatio = bounds.width / bounds.height;
+        if (rectRatio > boundsRatio) return [bounds.width, rect.height * (bounds.width / rect.width)]; // Fit to width if rect is more landscape than bounds
+        else return [rect.width * (bounds.height / rect.height), bounds.height]; // Fit to height if rect is more portrait than bounds
     }
 
     stop() {
